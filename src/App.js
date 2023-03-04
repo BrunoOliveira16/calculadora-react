2460a5bdc0f3f9892bbd6ac089f3705c39b56e90
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2023-03-04 15:36:38 -0300

    Finalizado js das operações básicas

--- src/App.js
+++ src/App.js
@@ -7,22 +7,89 @@ import { useState } from 'react'
 const App = () => {
   const [currentNumber, setCurrentNumber] = useState('0')
   const [firstNumber, setFirstNumber] = useState('0')
-
-  const handleAddNumber = (num) => {
-    setCurrentNumber (prev => `${num}${prev === '0' ? '' : prev}`)
+  const [operation, setOperation] = useState('')
+  
+  const handleOnClear = () => {
+    setCurrentNumber('0')
+    setFirstNumber('0')
+    setOperation('')
   }
 
-  const handleOnClear = () => {
-    setCurrentNumber('')
+  const handleAddNumber = (num) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
   }
 
   const handleSumNumbers = () => {
+
     if(firstNumber === '0') {
       setFirstNumber(String(currentNumber))
-      handleOnClear()
+      setCurrentNumber('0')
+      setOperation('+')
     } else {
       const sum = Number(firstNumber) + Number(currentNumber)
       setCurrentNumber(String(sum))
+      setOperation('')
+    }
+  }
+
+  const handleSubtractionNumbers = () => {
+
+    if(firstNumber === '0') {
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('-')
+    } else {
+      const subtraction = Number(firstNumber) - Number(currentNumber)
+      setCurrentNumber(String(subtraction))
+      setOperation('')
+    }
+  }
+
+  const handleMultiplicationNumbers = () => {
+
+    if(firstNumber === '0') {
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('x')
+    } else {
+      const multiplication = Number(firstNumber) * Number(currentNumber)
+      setCurrentNumber(String(multiplication))
+      setOperation('')
+    }
+  }
+
+  const handleDivisionNumbers = () => {
+
+    if(firstNumber === '0') {
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('/')
+    } else {
+      const division = Number(firstNumber) / Number(currentNumber)
+      setCurrentNumber(String(division))
+      setOperation('')
+    }
+  }
+
+  const handleEquals = () => {
+
+    if(firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
+      switch(operation){
+        case '+':
+          handleSumNumbers()
+          break
+        case '-':
+          handleSubtractionNumbers()
+          break
+        case 'x':
+          handleMultiplicationNumbers()
+          break
+        case '/':
+          handleDivisionNumbers()
+          break
+        default:
+          break
+      }
     }
   }
 
@@ -38,14 +105,14 @@ const App = () => {
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="5" onClick={() => handleAddNumber('5')}/>
           <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="x" onClick={() => handleAddNumber('x')}/>
+          <Button label="x" onClick={handleMultiplicationNumbers}/>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="-" onClick={() => handleAddNumber('-')}/>
+          <Button label="-" onClick={handleSubtractionNumbers}/>
           <Button label="0" onClick={() => handleAddNumber('0')}/>
-          <Button label="=" onClick={() => handleAddNumber('=')}/>
-          <Button label="/" onClick={() => handleAddNumber('/')}/>
+          <Button label="=" onClick={handleEquals}/>
+          <Button label="/" onClick={handleDivisionNumbers}/>
           <Button label="+" onClick={handleSumNumbers}/>
         </ContentButtons>
       </Content>
